b7235a7eb6f174a6369f7d18fb6b22ccca14941e
RHays91 <rhaysvm42@gmail.com>
2015-07-16 12:29:18 -0700

    Setting up Queue view

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,6 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
   
-
   initialize: function(){
     this.on('add', this.addHandler, this);
     this.on('ended', this.removeSong, this);
